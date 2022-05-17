89e3d71b19ff0b7ae704fb1dd92eff9ba6b8bb7a
root <admin@example.com>
2022-05-18 00:16:13 +0900

    CI/CD

--- hello.py
+++ hello.py
@@ -1,14 +1,15 @@
-from cmd import PROMPT
 import click
 
-@click.command(help="This is just hello app. Is does nothing")
+@click.command(help="This is just a hello app. It does nothing.")
 @click.option("--name", prompt="I need your name", help="Need name")
 @click.option("--color", prompt="I need your color", help="This is your color")
 def hello(name, color):
     if name == "Thor":
-        click.echo("Thor your name are always red")
-        click.echo(click.style(f"Hello World! {name}", fg="red"))
+        click.echo("Thor, you are always red.")
+        click.echo(click.style(f"Hello {name}!", fg="red"))
     else:
-        click.echo(click.style(f"Hello World! {name}", fg=color))
+        click.echo(f"Your color is {color}!")
+        click.echo(click.style(f"Hello {name}!", fg=color))
+
 if __name__ == "__main__":
     hello()
